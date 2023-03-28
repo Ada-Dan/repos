806791ef30caca09dbfcbdd22b4e3d8aee4b3483
BewleyRichard <118364533+BewleyRichard@users.noreply.github.com>
2023-03-28 22:23:15 +0100

    initial

---
+++ BookingApp/api/controllers/authController.js
@@ -0,0 +1,117 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
+/* Creates a new user. 
+Uses bcrypt to encrypt password. */
+
+export const register = async (req, res, next) => {
+    try {
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt)
+        const newUser = new User ({
+            username:req.body.username, 
+            email:req.body.email,
+            password:hash,
+        })
+        await newUser.save()
+        res.status(201).send("User registration complete")
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne(({username:req.body.username}))
+        if (!user) return next (createError(404, "user not found")) // may need to get rid of the next here.
+
+
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password,
+            user.password);
+        
+        if (!isPasswordCorrect) 
+        return next (createError(400, "Wrong username or password"));
+
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.SECRET) //hide in .env later
+        
+        const { password, isAdmin, ...otherDetails} = user._doc;
+        
+
+        res
+        .cookie("access_token", token, {
+            httpOnly:true,
+            
+        })
+        .status(200).json({...otherDetails});
+    } catch (err) {
+        next(err)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     try {
+//         const salt = bcrypt.genSaltSync(10);
+//         const hash = bcrypt.hashSync(req.body.password, salt);
+
+
+//         const newUser = new User({
+//             ...req.body,
+//             password: hash,
+//         });
+
+//         await newUser.save();
+//         res.status(200).send("User has been created");
+//     } catch (err) {
+//         next(err);
+//     }
+// };
+
+// /* User login autheticates username and password. 
+// If password is correct a jwt is generated. */
+
+// export const login = async (req, res, next) => {
+
+//     try {
+//         const user = await User.findOne({ username: req.body.username });
+//         if(!user) return next(createError(404, "User not found" ));
+
+//         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+//         if(!isPasswordCorrect) return next(createError(400, "Wrong username or password" ))
+
+//         const token = jwt.sign(
+//             { id: user._id, isAdmin: user.isAdmin }, 
+//             process.env.JWT);
+
+//         const { password, isAdmin, ...otherDetails} = user._doc;
+
+        
+//         res.status(200).json({ token: token, details: { ...otherDetails }, isAdmin });
+
+//             res.cookie("access_token", token, {
+//             httpOnly: true,
+//          })
+//         .status(200)
+//         .json({ details: { ...otherDetails }, isAdmin });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+
---
+++ BookingApp/client/src/pages/Home/home.js
@@ -0,0 +1,30 @@
+import Navbar from "../../components/navbar/Navbar.js";
+import Header from "../../components/header/Header.js";
+import Featured from "../../components/featured/Featured.js";
+
+import "./home.css";
+import PropertyList from "../../components/propertyList/PropertyList.js";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties.js";
+import MailList from "../../components/mailList/MailList.js";
+import Footer from "../../components/footer/Footer.js";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Header/>
+            <div className="homeContainer">
+                <Featured/>
+                <h1 className="homeTitle">Browse by property type</h1>
+                <PropertyList/>
+                <h1 className="homeTitle">Homes guests love</h1>
+                <FeaturedProperties/>
+                <MailList/>
+                <Footer/>
+            </div>
+        </div>
+
+    );
+};
+
+export default Home;
